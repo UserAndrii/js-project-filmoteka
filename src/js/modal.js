4a1bd7126f1f6913a4ac32a9651e2964d79b22fe
SlavaKholod <kholod.v.job@gmail.com>
2023-04-13 21:31:00 +0300

    add modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -101,16 +101,16 @@ if (!Math.ceil10) {
   };
 }
 
-document.getElementById('btn-modal').addEventListener('click', function() {
-  document.getElementById('overlay').classList.add('is-visible');
-  document.getElementById('modal').classList.add('is-visible');
-});
+// document.getElementById('btn-modal').addEventListener('click', function() {
+//   document.getElementById('backdrop').classList.add('is-visible');
+//   document.getElementById('modal').classList.add('is-visible');
+// });
 
 document.getElementById('close-btn').addEventListener('click', function() {
-  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('backdrop').classList.remove('is-visible');
   document.getElementById('modal').classList.remove('is-visible');
 });
-document.getElementById('overlay').addEventListener('click', function() {
-  document.getElementById('overlay').classList.remove('is-visible');
+document.getElementById('backdrop').addEventListener('click', function() {
+  document.getElementById('backdrop').classList.remove('is-visible');
   document.getElementById('modal').classList.remove('is-visible');
 });
