681686590ec280e18014ae30c09e53d360fb0959
UserAndrii <113310900+UserAndrii@users.noreply.github.com>
2023-04-13 20:06:56 +0000

    Deploying to gh-pages from @ UserAndrii/js-project-filmoteka@ff60131b06a7d1eae5e17ff6ba462a73afd8b066 🚀

---
+++ index.0d0fc6ce.js
@@ -0,0 +1,2 @@
+var refs={heroWrapper:document.querySelector(".hero__wrapper"),btnWatched:document.querySelector(".hero__btn-watched"),btnLibrary:document.querySelector(".btn_library")};refs.btnLibrary.addEventListener("click",(function(e){refs.heroWrapper.style.justifyContent="space-between"}));
+//# sourceMappingURL=index.0d0fc6ce.js.map
