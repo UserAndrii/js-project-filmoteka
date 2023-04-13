deba99ef5a1e6de6fe3cd9bd5f4e10e68629bf9e
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-04-13 10:58:57 +0200

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-modal').addEventListener('click', function() {
+  document.getElementById('overlay').classList.add('is-visible');
+  document.getElementById('modal').classList.add('is-visible');
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
+});
+document.getElementById('overlay').addEventListener('click', function() {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
+});
