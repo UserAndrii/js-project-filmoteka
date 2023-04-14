a0287fde57dcc25fd3106e89506d14cd8749c806
Andrii Gadar <gadar.andre@gmail.com>
2023-04-14 09:21:57 +0300

    fixed to import html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const API_KEY = '58645e23389326a2e8ed75695b9e1b79';
 // import './js/modal';
 import './js/fetch-by-search';
+import './js/library-header-buttons';
 // import './js/team-modal';
 import './js/main-gallery';
