c75c80569ee227dc038cbfe20275fadf6bf9d3a9
GalynkaK <gkarpinskaya@gmail.com>
2023-04-17 13:59:37 +0300

    add video

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -10,156 +10,157 @@ import Natalia from '../images/team/natalia.jpg';
 import Slava from '../images/team/slava.jpg';
 import Oleksandr from '../images/team/oleksandr.jpg';
 import Andriy from '../images/team/andriy.jpg';
-import video from '../images/team/-32298.mp4';
+// import video from '../images/team/-32298.mp4';
 import svgGit from '../images/gitsvg.svg';
 import svgLinkd from '../images/linkedin.svg';
 
 
-const markup = `<div class="intro">
-      <div class="video">
-        <video class="video__media" src="${video}" autoplay muted loop ></video>
+const markup =
+  `<div class="intro">
+    <div class="video">
+      <video class="video__media" src="../images/team/-32298.mp4" autoplay muted loop></video>
+    </div>
+    <div class="intro__content team-wrapper">
+        <div class="team-card">
+            <img src="${Nataliia}" alt="Nataliia" class="team-image">
+            <p class="team-name">Nataliia</p>
+            <p class="team-name">Valko</p>
+            <p class="team-role">Mentor JS</p>
+            <p class="logo__icon-footer">
+            <a href="https://github.com/NATALIIAVALKO" target="_blank" class="team-git"><svg width="24" height="24">
+              <use href="${svgGit}#icon-github-svgr"></use></svg></a>
+            <a href="https://www.linkedin.com/in/nataliia-valko-951501212/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+              <use href="${svgLinkd}#icon-linkedin-svg"></use>
+            </svg></a>
+            </p>
+        </div>
+        <div class="team-card">
+          <img src="${Andrii}" alt="Andrii" class="team-image">
+          <p class="team-name">Andrii</p>
+          <p class="team-name">Gadar</p>
+          <p class="team-role">Team Lead</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/UserAndrii" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/andrii-hadar/" target="_blank" class="team-linkd"><svg width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
       </div>
-      <div class="intro__content team-wrapper">
-<div class="team-card">
-    <img src="${Nataliia}" alt="Nataliia" class="team-image">
-    <p class="team-name">Nataliia</p>
-    <p class="team-name">Valko</p>
-    <p class="team-role">Mentor JS</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/NATALIIAVALKO" target="_blank" class="team-git"><svg width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use></svg></a>
-    <a href="https://www.linkedin.com/in/nataliia-valko-951501212/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Andrii}" alt="Andrii" class="team-image">
-    <p class="team-name">Andrii</p>
-    <p class="team-name">Gadar</p>
-    <p class="team-role">Team Lead</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/UserAndrii" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/andrii-hadar/" target="_blank" class="team-linkd"><svg width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Yura}" alt="" class="team-image">
-    <p class="team-name">Yurii</p>
-    <p class="team-name">Kahadii</p>
-    <p class="team-role">Scrum Master</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/yurii-kahadii-79405a272/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Galyna}" alt="Galyna" class="team-image">
-    <p class="team-name">Galyna</p>
-    <p class="team-name">Karpinska</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/GalynkaK" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/galia-karpinskaya-89842b254/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Maryna}" alt="Matyna" class="team-image">
-    <p class="team-name">Maryna</p>
-    <p class="team-name">Khmarska</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/myhappyday" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/%D0%BC%D0%B0%D1%80%D0%B8%D0%BD%D0%B0-%D1%85%D0%BC%D0%B0%D1%80%D1%81%D0%BA%D0%B0%D1%8F-4b9903253/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Yuriy}" alt="Yuriy" class="team-image">
-    <p class="team-name">Yuriy</p>
-    <p class="team-name">Klumenko</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Natalia}" alt="Natalia" class="team-image">
-    <p class="team-name">Natalia</p>
-    <p class="team-name">Kuksa</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/" target="_blank" class="team-git"><svg width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Oleksandr}" alt="" class="team-image">
-    <p class="team-name">Oleksandr</p>
-    <p class="team-name">Deren</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="hhttps://github.com/Oderen?tab=repositories" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Slava}" alt="Slava" class="team-image">
-    <p class="team-name">Slava</p>
-    <p class="team-name">Kholod</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="linkedin.com/in/slava-kholod-dev" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-<div class="team-card">
-    <img src="${Andriy}" alt="Andriy" class="team-image">
-    <p class="team-name">Andriy</p>
-    <p class="team-name">Hanzel</p>
-    <p class="team-role">Developer</p>
-    <p class="logo__icon-footer">
-    <a href="https://github.com/andriy-h80" target="_blank" class="team-git"><svg width="24" height="24">
-      <use href="${svgGit}#icon-github-svgr"></use>
-    </svg></a>
-    <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg  width="24" height="24">
-      <use href="${svgLinkd}#icon-linkedin-svg"></use>
-    </svg></a>
-    </p>
-</div>
-</div>
+      <div class="team-card">
+          <img src="${Yura}" alt="" class="team-image">
+          <p class="team-name">Yurii</p>
+          <p class="team-name">Kahadii</p>
+          <p class="team-role">Scrum Master</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/yurii-kahadii-79405a272/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Galyna}" alt="Galyna" class="team-image">
+          <p class="team-name">Galyna</p>
+          <p class="team-name">Karpinska</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/GalynkaK" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/galia-karpinskaya-89842b254/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Maryna}" alt="Matyna" class="team-image">
+          <p class="team-name">Maryna</p>
+          <p class="team-name">Khmarska</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/myhappyday" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/%D0%BC%D0%B0%D1%80%D0%B8%D0%BD%D0%B0-%D1%85%D0%BC%D0%B0%D1%80%D1%81%D0%BA%D0%B0%D1%8F-4b9903253/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Yuriy}" alt="Yuriy" class="team-image">
+          <p class="team-name">Yuriy</p>
+          <p class="team-name">Klumenko</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Natalia}" alt="Natalia" class="team-image">
+          <p class="team-name">Natalia</p>
+          <p class="team-name">Kuksa</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/" target="_blank" class="team-git"><svg width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Oleksandr}" alt="" class="team-image">
+          <p class="team-name">Oleksandr</p>
+          <p class="team-name">Deren</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="hhttps://github.com/Oderen?tab=repositories" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Slava}" alt="Slava" class="team-image">
+          <p class="team-name">Slava</p>
+          <p class="team-name">Kholod</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/" target="_blank" class="team-git"><svg  width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="linkedin.com/in/slava-kholod-dev" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+      <div class="team-card">
+          <img src="${Andriy}" alt="Andriy" class="team-image">
+          <p class="team-name">Andriy</p>
+          <p class="team-name">Hanzel</p>
+          <p class="team-role">Developer</p>
+          <p class="logo__icon-footer">
+          <a href="https://github.com/andriy-h80" target="_blank" class="team-git"><svg width="24" height="24">
+            <use href="${svgGit}#icon-github-svgr"></use>
+          </svg></a>
+          <a href="https://www.linkedin.com/in/" target="_blank" class="team-linkd"><svg  width="24" height="24">
+            <use href="${svgLinkd}#icon-linkedin-svg"></use>
+          </svg></a>
+          </p>
+      </div>
+  </div>
 </div>`;
 
 const teamButton = document.querySelector('.js-team-modal');
